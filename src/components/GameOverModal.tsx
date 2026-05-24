6640cf818c202ff79ae16b6c06a96e65309842ea
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-24 09:23:46 +0200

    refactor: rename ModalProps to GameOverModalProps

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { cn } from "../lib/cn";
 
-type ModalProps = {
+type GameOVerModalProps = {
   isOpen: boolean;
   score: number[];
   moves: number;
@@ -23,7 +23,7 @@ function GameOverModal({
   moves,
   score,
   rankedPlayers,
-}: ModalProps) {
+}: GameOVerModalProps) {
   const dialogRef = useRef<HTMLDialogElement>(null);
   const winners = rankedPlayers.filter(
     (winner) => winner.score === rankedPlayers[0].score,
