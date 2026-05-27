7f960c3d6a4c58be17e43b28f740bae7479c871c
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-27 07:03:28 +0200

    feat: add ErrorBoundary component to handle React errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import StartScreen from "./pages/StartScreen";
 import GameBoard from "./pages/GameBoard";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartScreen />} />
-        <Route path="/game" element={<GameBoard />} />
+        <Route
+          path="/game"
+          element={
+            <ErrorBoundary
+              fallback={<p>Une erreur est survenue. Rechargez la page.</p>}
+            >
+              <GameBoard />
+            </ErrorBoundary>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
